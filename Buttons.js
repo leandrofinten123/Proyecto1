ee9c052b1ea5f89b59d231bb1b2e238fb1e59b85
Leandro <leandro_finten@hotmail.com>
2024-05-25 20:17:59 -0300

    prueba

---
+++ Buttons.js
@@ -0,0 +1,35 @@
+
+
+const botonMenu = document.querySelector(".button__1");
+const botonCerrar = document.querySelector(".botoncerrar");
+const menu = document.querySelector(".menudesplegable");
+
+botonMenu.addEventListener("click", function() {
+  menu.classList.toggle("mostrar");
+});
+
+botonCerrar.addEventListener("click", function() {
+  menu.classList.remove("mostrar");
+});
+
+const boton2Menu = document.querySelector(".button__2");
+const boton2Cerrar = document.querySelector(".botoncerrar2");
+const menu2 = document.querySelector(".menudesplegable2");
+
+boton2Menu.addEventListener("click", function() {
+  menu2.classList.toggle("mostrar");
+});
+
+boton2Cerrar.addEventListener("click", function() {
+  menu2.classList.remove("mostrar");
+});
+
+fetch('https://my-json-server.typicode.com/juan123/mi-json-api/persona')
+            .then(response => response.json())
+            .then(data => {
+                const contenidoHeader = document.getElementById('titulo__1');
+                contenidoHeader.innerHTML = `
+                <img src="${data.imagen1}" alt ="">
+                `;
+            })
+            .catch(error => console.error('Error al leer el archivo JSON:', error));
